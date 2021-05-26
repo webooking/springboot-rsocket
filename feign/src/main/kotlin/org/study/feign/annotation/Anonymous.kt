01ad688e70e197b422122cfb375148fe9b1e47f0
yuri-li <javaliyuanyuan@163.com>
2021-05-26 09:26:51 +0800

    Delete feign directory

--- feign/src/main/kotlin/org/study/feign/annotation/Anonymous.kt
+++
@@ -1,6 +0,0 @@
-package org.study.feign.annotation
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention
-@MustBeDocumented
-annotation class Anonymous
--- feign/src/main/kotlin/org/study/feign/annotation/RSocketClient.kt
+++
@@ -1,6 +0,0 @@
-package org.study.feign.annotation
-
-@Retention
-@Target(AnnotationTarget.CLASS)
-@MustBeDocumented
-annotation class RSocketClient
